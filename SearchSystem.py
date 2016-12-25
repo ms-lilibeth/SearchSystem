11ac9d1892f4dcffff74930a74103478705b02a1
ms-lilibeth <ms.lilibeth@yandex.ru>
2016-12-25 19:58:46 +0500

    Task finished

--- SearchSystem.py
+++ SearchSystem.py
@@ -1,8 +1,8 @@
 import Stemmer
-import sklearn
 import re
 import pickle
-
+from collections import Counter, defaultdict
+import math
 
 class Searcher:
     @staticmethod
@@ -20,9 +20,36 @@ class Searcher:
         stop_words = pickle.load(open("stop-words-binary",'rb'))
         return [w for w in text if w not in stop_words]
 
-    def get_tf_idf_features(text):
-        pass
+    def __init__(self, files):
+        self.data = {}
+        for file in files:
+            with open(file, encoding='utf-8') as f:
+                text = self.lexical_analysis(f.read())
+                text = self.stemming(self.delete_stop_words(text))
+                self.data[file] = Counter(text)
+
+    def tf(self, term, file):
+        return self.data[file][term] / sum(self.data[file].values())
+
+    def idf(self, term):
+        count = 0
+        for stat in self.data.values():
+            if term in stat:
+                count += 1
+        return 0 if count == 0 else math.log(len(self.data) / count)
+
+    def tf_idf(self, term, file):
+        return self.tf(term, file) * self.idf(term)
+
+    def search(self, query):
+        terms = set(self.stemming(self.delete_stop_words(self.lexical_analysis(query))))
+        stats = defaultdict(lambda: 0)
+        for term in terms:
+            for file in self.data:
+                stats[file] += self.tf_idf(term, file)
+        return stats
 
 
 if __name__=="__main__":
+    pass
     # print(lexical_analysis("Мама: мыла раму!"))
--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from os.path import isfile, join
 dir_name = "text_collection/"
 if __name__=="__main__":
     files = [join(dir_name, f) for f in listdir(dir_name) if isfile(join(dir_name, f))]
-
+    searcher = Searcher(files)
+    query = input('Query: ')
+    stats = sorted(searcher.search(query).items(), key=lambda x: x[1], reverse=True)
+    for file, s in stats:
+        print(file, ": ", s)
     # filename = "sometext.txt"
 
     # with open(filename, 'r') as f:
